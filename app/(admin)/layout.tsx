3a66040ef64512a3b19abbf6175948d2d7c81372
Kapren34 <kapren34@gmail.com>
2025-10-28 13:17:47 +0200

    fix(build): (admin) altÄ±ndaki tÃ¼m sayfalarÄ± dynamic render'a geÃ§ir

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,5 +1,9 @@
 'use client'
 
+export const dynamic = 'force-dynamic'
+export const fetchCache = 'force-no-store'
+export const revalidate = 0
+
 import Link from 'next/link'
 import { usePathname, useRouter } from 'next/navigation'
 import { useState } from 'react'
